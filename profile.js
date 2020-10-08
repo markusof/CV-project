7aa892436d1dec8209f25980f0533690e54f071b
Markusof <markus.oien.frydenberg@gmail.com>
2020-10-08 11:18:02 +0200

    Made some changes to profile.html and am trying to create react-functionality with js

---
+++ profile.js
@@ -0,0 +1,12 @@
+import React, { useState } from 'react'
+
+const ProfileApp = () => {
+    const [active, setActive] = useState(" ")
+    return (
+        <p>
+            This is Markus
+        </p>
+    )
+}
+
+export default ProfileApp
